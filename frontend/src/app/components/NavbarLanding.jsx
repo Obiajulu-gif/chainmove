20dbaf4b82b7d89efe10ef2d8b3d186ca8f8dfe5
Emmanuel Obiajulu Okoye <64269783+Obiajulu-gif@users.noreply.github.com>
2024-11-07 17:36:22 +0000

    dashboad done

--- frontend/src/app/components/NavbarLanding.jsx
+++ frontend/src/app/components/NavbarLanding.jsx
@@ -1,90 +1,93 @@
 "use client";
-import { useState } from "react";
+
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/navigation"; // Import for navigation
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+// Import for navigation
 import { FaBars, FaTimes } from "react-icons/fa";
 
 const NavbarLanding = () => {
-	const [isOpen, setIsOpen] = useState(false);
-	const router = useRouter(); // Initialize router
+  const [isOpen, setIsOpen] = useState(false);
+  const router = useRouter(); // Initialize router
 
-	const toggleMenu = () => {
-		setIsOpen(!isOpen);
-	};
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
-	const navigateToSign = () => {
-		router.push("/register"); // Navigate to dashboard
-		setIsOpen(false); // Close the menu on navigation
-	};
+  const navigateToSign = () => {
+    router.push("/register"); // Navigate to register page
+    setIsOpen(false); // Close the menu on navigation
+  };
 
-	return (
-		<nav className="bg-gradient-to-r from-gray-800 to-gray-900 p-4 fixed w-full z-20">
-			<div className="container mx-auto flex items-center justify-between">
-				{/* Logo and Name */}
-				<div className="flex items-center">
-					<Image
-						src="/images/blockridelogo.svg"
-						alt="ChainMove Logo"
-						width={40}
-						height={40}
-						className="mr-2 animate-pulse"
-					/>
-					<span className="text-white text-xl font-semibold">Chain<span className="text-orange-500">Move</span></span>
-				</div>
+  return (
+    <nav className="bg-gradient-to-r from-gray-800 to-gray-900 p-4 fixed w-full z-20">
+      <div className="container mx-auto flex items-center justify-between">
+        {/* Logo and Name */}
+        <div className="flex items-center">
+          <Image
+            src="/images/blockridelogo.svg"
+            alt="ChainMove Logo"
+            width={40}
+            height={40}
+            className="mr-2 animate-pulse"
+          />
+          <span className="text-white text-xl font-semibold">
+            Chain<span className="text-orange-500">Move</span>
+          </span>
+        </div>
 
-				{/* Menu Links */}
-				<div className="hidden md:flex space-x-8 text-white">
-					<Link href="/">Home</Link>
-					<Link href="https://www.notion.so/12f385cd77cc8041a9fefc57981d5ea8?pvs=4">
-						About Us
-					</Link>
-					<Link href="/about">Contact Us</Link>
-				</div>
+        {/* Menu Links */}
+        <div className="hidden md:flex space-x-8 text-white">
+          <Link href="/" as="/index.html">
+            Home
+          </Link>
+          <Link href="https://www.notion.so/12f385cd77cc8041a9fefc57981d5ea8?pvs=4" as="/about-us.html">
+            About Us
+          </Link>
+          <Link href="/about" as="/about.html">
+            Contact Us
+          </Link>
+        </div>
 
-				{/* Start a Journey Button */}
-				<button
-					onClick={navigateToSign}
-					className="hidden md:block bg-orange-500 text-white px-4 py-2 rounded-lg animate-bounce"
-				>
-					Start a journey
-				</button>
+        {/* Start a Journey Button */}
+        <button
+          onClick={navigateToSign}
+          className="hidden md:block bg-orange-500 text-white px-4 py-2 rounded-lg animate-bounce">
+          Start a journey
+        </button>
 
-				{/* Hamburger Menu Icon */}
-				<div
-					className="md:hidden text-white cursor-pointer"
-					onClick={toggleMenu}
-				>
-					{isOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
-				</div>
-			</div>
+        {/* Hamburger Menu Icon */}
+        <div className="md:hidden text-white cursor-pointer" onClick={toggleMenu}>
+          {isOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+        </div>
+      </div>
 
-			{/* Mobile Menu */}
-			{isOpen && (
-				<div className="md:hidden bg-gray-800 text-white">
-					<Link href="/" onClick={toggleMenu} className="block px-4 py-2">
-						Home
-					</Link>
-					<Link
-						href="https://www.notion.so/12f385cd77cc8041a9fefc57981d5ea8?pvs=4"
-						onClick={toggleMenu}
-						className="block px-4 py-2"
-					>
-						About Us
-					</Link>
-					<Link href="/about" onClick={toggleMenu} className="block px-4 py-2">
-						Contact Us
-					</Link>
-					<button
-						onClick={navigateToSign}
-						className="w-full bg-orange-500 text-white px-4 py-2 mt-2 rounded-lg"
-					>
-						Start a journey
-					</button>
-				</div>
-			)}
-		</nav>
-	);
+      {/* Mobile Menu */}
+      {isOpen && (
+        <div className="md:hidden bg-gray-800 text-white">
+          <Link href="/" as="/index.html" onClick={toggleMenu} className="block px-4 py-2">
+            Home
+          </Link>
+          <Link
+            href="https://www.notion.so/12f385cd77cc8041a9fefc57981d5ea8?pvs=4"
+            as="/about-us.html"
+            onClick={toggleMenu}
+            className="block px-4 py-2">
+            About Us
+          </Link>
+          <Link href="/about" as="/about.html" onClick={toggleMenu} className="block px-4 py-2">
+            Contact Us
+          </Link>
+          <button
+            onClick={navigateToSign}
+            className="w-full bg-orange-500 text-white px-4 py-2 mt-2 rounded-lg">
+            Start a journey
+          </button>
+        </div>
+      )}
+    </nav>
+  );
 };
 
 export default NavbarLanding;
