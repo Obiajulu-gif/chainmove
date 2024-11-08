4dd5d1b4800347d6f027ff862ac21267f71ece4d
Emmanuel Obiajulu Okoye <64269783+Obiajulu-gif@users.noreply.github.com>
2024-11-08 00:27:21 +0000

    in progress

---
+++ frontend/src/app/components/header.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = Header;
+var ares_react_1 = require("@bundly/ares-react");
+function Header() {
+  return (
+    <>
+      <ares_react_1.InternetIdentityButton />
+    </>
+  );
+}
---
+++ frontend/src/app/components/header.tsx
@@ -0,0 +1,9 @@
+import { InternetIdentityButton } from "@bundly/ares-react";
+
+export default function Header() {
+  return (
+    <>
+      <InternetIdentityButton />
+    </>
+  );
+}
